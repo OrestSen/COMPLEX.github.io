7f213b904fa4f596eae8e8d8d089c42de2add177
orest <oresttrue@gmail.com>
2020-09-14 12:53:34 +0300

    added tab with js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
-$(function() {
 
-$('.slider__inner, .news__slider-inner').slick({
-    nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
-    prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
-    infinite: false
-    
+$(function () {
+
+    $('.slider__inner, .news__slider-inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+
+    });
+
+    $('select').styler();
+
+    $('.header__btn-menu').on('click', function () {
+        $('.menu ul').slideToggle();
+    })
+
 });
+let tabNav = document.querySelectorAll(".tabs__item")
+let tabContent = document.querySelectorAll(".tab")
+let tabName = 0
 
-$('select').styler();
+function add() {
+    tabNav.forEach(function(item) {
+        item.addEventListener("click", selectTabNav)
+    });
+    function selectTabNav() {
+        tabNav.forEach(function(item) {
+            item.classList.remove("active");
+        });
+        this.classList.add("active");
+        tabName = this.getAttribute("data-tab-name");
+        selectTabName(tabName)
+    }
+    function selectTabName(tabName) {
+        tabContent.forEach(function(item)  {
+            if(item.classList.contains(tabName)) {
+                item.classList.add("active")
+            } else item.classList.remove("active");
+        })
+    }
+  };
+add();
 
-$('.header__btn-menu').on('click', function() {
-    $('.menu ul').slideToggle();
-})
-
-});
